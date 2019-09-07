4aa2ec6ad59f453545d570db1754cc1c3757c3c9
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-09-07 13:06:26 +0200

    persist measured data in redis

--- enviroplus/demo.py
+++ enviroplus/demo.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import logging
 import math
 import time
 import sys
@@ -11,10 +12,17 @@ from bme280 import BME280
 from smbus2 import SMBus
 from enviroplus import gas
 
+import redis
 import numpy as np
+
+logging.basicConfig(
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
+redis = redis.Redis()
 
 def get_cpu_temp():
     proc = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
@@ -54,13 +62,28 @@ try:
         l = np.median(lux)
         cpu = get_cpu_temp()
 
+        ts = datetime.timestamp(datetime.now())
+        redis.zadd('temperature_raw', {ts: t})
+        redis.zadd('pressure', {ts: p})
+        redis.zadd('humidity', {ts: h})
+        redis.zadd('nh3', {ts: n})
+        redis.zadd('oxidising', {ts: o})
+        redis.zadd('reducing', {ts: r})
+        redis.zadd('light', {ts: l})
+        redis.zadd('temperature_cpu', {ts: cpu})
         if cpu > t:
             approx = t - (cpu - t) * 0.3
-            print('corrected temp: {:.1f}°C'.format(approx))
+            logging.info('corrected temp={:.1f}°C'.format(approx))
+            redis.zadd('temperature_corrected', {ts: approx})
 
-        print('cpu={:.1f}°C, temperature={:.1f}°C, pressure={:.1f} HPa, humidity={:.2f}%'.format(cpu, t, p, h))
-        print('nh3 level={:.3f}, oxidising={:.3f}, reducing={:.3f}'.format(n, o, r))
-        print('light={:.3f} lux'.format(l))
+        logging.info('cpu={:.1f}°C'.format(cpu))
+        logging.info('temperature={:.1f}°C'.format(t))
+        logging.info('pressure={:.1f} HPa'.format(p))
+        logging.info('humidity={:.2f}%'.format(h))
+        logging.info('nh3 level={:.3f}'.format(n))
+        logging.info('oxidising={:.3f}'.format(o))
+        logging.info('reducing={:.3f}'.format(r))
+        logging.info('light={:.3f} lux'.format(l))
 
         end = datetime.timestamp(datetime.now())
         diff = end - start
